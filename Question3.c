d19e59700a820d32ed5ac8d7023adac87f2bdd84
naman1gupta <72181247+naman1gupta@users.noreply.github.com>
2021-10-01 11:29:26 +0530

    Create Question3.c

---
+++ Question3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int f(int a,int b){
+    if(b=a-1){}
+    while(b>0){ if(a*=b--){} }
+    if(printf("%d",a)){}
+}
+int main(int argc,char* argv[]){
+    if(argc=atoi(argv[1])){}
+    if(f(argc,1)){}
+}
